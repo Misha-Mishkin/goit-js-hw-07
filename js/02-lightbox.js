259ad0d876b1a69dd2c4207883d2917cda3750a5
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-04-02 17:21:05 +0300

    Второе задание сделано

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+
+const listGallery = createGallery(galleryItems);
+
+galleryContainer.innerHTML = listGallery;
+
+
+function createGallery(items) {
+    return items.map(({ preview, original, description }) => `
+<li>
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image"
+  src="${preview}"
+  alt="${description}" />
+</a>
+</li>`).join('');
+    
+}
+
+let gallery = new SimpleLightbox('.gallery a',
+         {
+             navText: ['<', '>'],
+             captionsData: 'alt',    
+             captionPosition: 'bottom',
+             captionDelay: 250,
+
+         });
+    
+    console.log(gallery);
+
+
+
